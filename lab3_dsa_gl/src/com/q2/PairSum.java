9934fde298f69f692623dd0d9e9ccc6e9cf111da
Aryansh0000 <82965560+Aryansh0000@users.noreply.github.com>
2022-05-02 18:37:50 +0530

    Add files via upload

---
+++ lab3_dsa_gl/src/com/q2/PairSum.java
@@ -0,0 +1,61 @@
+package com.q2;
+import java.util.*;
+
+class Node {
+	int nodeData;
+	Node leftNode , rightNode;
+}
+class PairSum {
+	static Node NewNode(int nodeData) {
+		Node temp = new Node();
+		temp.nodeData =nodeData;
+		temp.leftNode = null;
+		temp.rightNode = null;
+		return temp;
+	}
+	public Node insert(Node root , int key) {
+		if(root==null)
+			return NewNode(key);
+		if(key<root.nodeData)
+			root.leftNode = insert(root.leftNode, key);
+		else 
+			root.rightNode = insert(root.rightNode, key);
+		return root;
+	}
+	
+	public boolean findpairUtil(Node root, int sum, HashSet<Integer> set) {
+		if(root == null)
+			return false;
+		if(findpairUtil(root.leftNode, sum, set))
+			return true;
+		if(set.contains(sum-root.nodeData)) {
+			System.out.println("Pair is found (" + (sum-root.nodeData) + "," + root.nodeData + ")");
+			return true;
+		}
+		else
+			set.add(root.nodeData);
+		return findpairUtil(root.rightNode, sum, set);
+	}
+	public void findPairWithGivenSum(Node root, int sum) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		if(!findpairUtil(root, sum, set))
+			System.out.print("Pairs do not exit" + "\n");
+	}
+	
+
+	public static void main(String[] args) {
+		Node root = null;
+		PairSum findSP = new PairSum();
+		root =findSP.insert(root, 40);
+		root =findSP.insert(root, 20);
+		root =findSP.insert(root, 60);
+		root =findSP.insert(root, 10);
+		root =findSP.insert(root, 30);
+		root =findSP.insert(root, 50);
+		root =findSP.insert(root, 70);
+		int sum = 130;
+		findSP.findPairWithGivenSum(root, sum);
+		
+	}
+	
+}
